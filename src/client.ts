40cc6e7e7a8265bf3825f1abcac58379654a19ef
Timothy Cole <tim@timcole.me>
2018-09-23 22:25:39 -0400

    Moved things to mixins

--- src/client.ts
+++ src/client.ts
@@ -8,9 +8,13 @@ import { IYouTubeStats } from './interfaces/ytstatsdata.interface';
 
 import { Endpoints } from './utils/endpoints';
 
-export class SocialBladeClient {
-	private Http: RequestAPI<request.RequestPromise, request.RequestPromiseOptions, RequiredUriUrl>;
-	private Client: IClient;
+import { YouTube } from './mixins/index';
+const mixins = [ YouTube ];
+export class SocialBladeClient implements YouTube {
+	public $parent: SocialBladeClient = this;
+
+	public Http: RequestAPI<request.RequestPromise, request.RequestPromiseOptions, RequiredUriUrl>;
+	public Client: IClient;
 
 	private readonly Version: string = "v2";
 	private readonly BaseURL: string = `https://api.socialblade.com/${this.Version}`;
@@ -72,74 +76,11 @@ export class SocialBladeClient {
 		return body;
 	}
 
-	public async StatsYouTube (identifier: string): Promise<IYouTubeStats> {
-		const response = <RequestResponse> await this.Http.get({
-			uri: Endpoints.YouTubeStats,
-			qs: {
-				query: `statistics`,
-				username: identifier,
-				...this.Client
-			}
-		});
-		let body = <IYouTubeStats> response.body;
-
-		// Set proper types since everything is a string in the API.
-		// #region data
-		if (!body.data) return body;
-		// toDate
-		body.data.created_at = new Date(body.data.created_at);
-		// toBoolean
-		body.data.partner = !!+body.data.partner;
-		body.data.isVerified = !!+body.data.isVerified;
-		// toFloat
-		body.data.avgdailysubs = parseFloat(body.data.avgdailysubs.toString());
-		body.data.avgdailyviews = parseFloat(body.data.avgdailyviews.toString());
-		// toNumber
-		body.data.uploads = Number(body.data.uploads);
-		body.data.subs = Number(body.data.subs);
-		body.data.views = Number(body.data.views);
-		// #endregion data
-
-		// #region rank
-		if (!body.rank) return body;
-		// toNumber
-		body.rank.raw.rank = Number(body.rank.raw.rank);
-		body.rank.raw.sbrank = Number(body.rank.raw.sbrank);
-		body.rank.raw.viewsrank = Number(body.rank.raw.viewsrank);
-		body.rank.raw.countryrank = Number(body.rank.raw.countryrank);
-		body.rank.raw.channeltyperank = Number(body.rank.raw.channeltyperank);
-		// #endregion rank
-
-		// #region charts
-		if (!body.charts) return body;
-		// toNumber -- Subs
-		body.charts.subs.subs14 = Number(body.charts.subs.subs14)
-		body.charts.subs.subs30 = Number(body.charts.subs.subs30)
-		body.charts.subs.subs60 = Number(body.charts.subs.subs60)
-		body.charts.subs.subs90 = Number(body.charts.subs.subs90)
-		body.charts.subs.subs180 = Number(body.charts.subs.subs180)
-		body.charts.subs.subs365 = Number(body.charts.subs.subs365)
-		// Views
-		body.charts.views.views14 = Number(body.charts.views.views14)
-		body.charts.views.views30 = Number(body.charts.views.views30)
-		body.charts.views.views60 = Number(body.charts.views.views60)
-		body.charts.views.views90 = Number(body.charts.views.views90)
-		body.charts.views.views180 = Number(body.charts.views.views180)
-		body.charts.views.views365 = Number(body.charts.views.views365)
-		// toFloat -- Growth
-		body.charts.growth.subs = parseFloat(body.charts.growth.subs.toString());
-		body.charts.growth.views = parseFloat(body.charts.growth.views.toString());
-		// #endregion charts
-
-		// #region data_daily
-		if (!body.data_daily) return body;
-		body.data_daily.map(item => {
-			item.date = new Date(item.date);
-			item.subs = Number(item.subs);
-			item.views = Number(item.views);
-		})
-		// #endregion data_daily
-
-		return body;
-	}
+	StatsYouTube: (identifier: string) => Promise<IYouTubeStats>;
 }
+
+// Invoke Mixins
+const base: any = SocialBladeClient;
+mixins.map((mixin: any) => {
+	Object.getOwnPropertyNames(mixin.prototype).map(name => { base.prototype[name] = mixin.prototype[name]; });
+})
---
+++ src/mixins/youtube.mixin.ts
@@ -0,0 +1,80 @@
+import { SocialBladeClient } from "../client";
+
+import { RequestResponse } from 'request';
+import { IYouTubeStats } from '../interfaces/ytstatsdata.interface';
+import { Endpoints } from '../utils/endpoints';
+
+export class YouTube {
+	$parent: SocialBladeClient;
+
+	public async StatsYouTube (identifier: string): Promise<IYouTubeStats> {
+		const response = <RequestResponse> await this.$parent.Http.get({
+			uri: Endpoints.YouTubeStats,
+			qs: {
+				query: `statistics`,
+				username: identifier,
+				...this.$parent.Client
+			}
+		});
+		let body = <IYouTubeStats> response.body;
+
+		// Set proper types since everything is a string in the API.
+		// #region data
+		if (!body.data) return body;
+		// toDate
+		body.data.created_at = new Date(body.data.created_at);
+		// toBoolean
+		body.data.partner = !!+body.data.partner;
+		body.data.isVerified = !!+body.data.isVerified;
+		// toFloat
+		body.data.avgdailysubs = parseFloat(body.data.avgdailysubs.toString());
+		body.data.avgdailyviews = parseFloat(body.data.avgdailyviews.toString());
+		// toNumber
+		body.data.uploads = Number(body.data.uploads);
+		body.data.subs = Number(body.data.subs);
+		body.data.views = Number(body.data.views);
+		// #endregion data
+
+		// #region rank
+		if (!body.rank) return body;
+		// toNumber
+		body.rank.raw.rank = Number(body.rank.raw.rank);
+		body.rank.raw.sbrank = Number(body.rank.raw.sbrank);
+		body.rank.raw.viewsrank = Number(body.rank.raw.viewsrank);
+		body.rank.raw.countryrank = Number(body.rank.raw.countryrank);
+		body.rank.raw.channeltyperank = Number(body.rank.raw.channeltyperank);
+		// #endregion rank
+
+		// #region charts
+		if (!body.charts) return body;
+		// toNumber -- Subs
+		body.charts.subs.subs14 = Number(body.charts.subs.subs14)
+		body.charts.subs.subs30 = Number(body.charts.subs.subs30)
+		body.charts.subs.subs60 = Number(body.charts.subs.subs60)
+		body.charts.subs.subs90 = Number(body.charts.subs.subs90)
+		body.charts.subs.subs180 = Number(body.charts.subs.subs180)
+		body.charts.subs.subs365 = Number(body.charts.subs.subs365)
+		// Views
+		body.charts.views.views14 = Number(body.charts.views.views14)
+		body.charts.views.views30 = Number(body.charts.views.views30)
+		body.charts.views.views60 = Number(body.charts.views.views60)
+		body.charts.views.views90 = Number(body.charts.views.views90)
+		body.charts.views.views180 = Number(body.charts.views.views180)
+		body.charts.views.views365 = Number(body.charts.views.views365)
+		// toFloat -- Growth
+		body.charts.growth.subs = parseFloat(body.charts.growth.subs.toString());
+		body.charts.growth.views = parseFloat(body.charts.growth.views.toString());
+		// #endregion charts
+
+		// #region data_daily
+		if (!body.data_daily) return body;
+		body.data_daily.map(item => {
+			item.date = new Date(item.date);
+			item.subs = Number(item.subs);
+			item.views = Number(item.views);
+		})
+		// #endregion data_daily
+
+		return body;
+	}
+}
